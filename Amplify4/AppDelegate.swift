9f26fba412db5727425d1026873993d592de5286
wrengels@wisc.edu <wrengels@wisc.edu>
2015-01-06 17:34:18 -0600

    Initial Commit

---
+++ Amplify4/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Amplify4
+//
+//  Created by Bill Engels on 1/6/15.
+//  Copyright (c) 2015 Bill Engels. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
